8a3bb6b7251244ae92c79a70ef7db6f2501f279d
Steven Jimenez <stevenmjimenez@gmail.com>
2023-09-02 01:46:50 -0400

    Remove template code from lib-nnet

--- lib/nnet/src/lib.rs
+++ lib/nnet/src/lib.rs
@@ -8,18 +8,3 @@
 )]
 
 mod activation;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
